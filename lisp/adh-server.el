fb78c7095bab21d7b9abf5453b829e79d36fda2b
Vostranox <vostranox@gmail.com>
2026-04-18 08:28:25 +0900

    Update adh-server.el

--- lisp/adh-server.el
+++ lisp/adh-server.el
@@ -2,6 +2,11 @@
 
 (require 'server)
 
+(when (eq system-type 'windows-nt)
+  (setq server-use-tcp t)
+  (setq server-name "main-server")
+  (setq server-auth-dir (concat user-emacs-directory "server/")))
+
 (unless (daemonp)
   (when (server-running-p)
     (server-force-delete))
